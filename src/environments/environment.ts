5ec1a029fe0d47f85a4c72f2b91b7d91b0c369bd
dark <lucas.breitembach@gmail.com>
2018-06-20 01:46:23 -0300

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  carretosApiUrl: 'http://localhost:3000/'
+  production: true,
+  carretosApiUrl: 'https://carretos-api.herokuapp.com/'
 };
 
 /*
